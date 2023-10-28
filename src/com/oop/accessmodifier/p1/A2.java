7df52ab39308a4b4bacd799c9bf6140530b5627f
VanHau87 <nguyenvanh29@gmail.com>
2023-10-28 17:56:49 +0700

    Access modifier re-factor part 2

--- src/com/oop/accessmodifier/p1/A2.java
+++ src/com/oop/accessmodifier/p1/A2.java
@@ -1,24 +1,12 @@
 package com.oop.accessmodifier.p1;
 
-import java.util.UUID;
-/**
- * A1 và A2 ở cùng package, ngoại trừ private ra, còn lại điều có thể access
- * */
 public class A2 {
-	String dept;
+	String className;
 	
-	UUID getId() {
+	public void accessFrom() {
 		A1 a1 = new A1();
-		return a1.getId();//id là private, khg thể access, phải thông qua public method
-	}
-	
-	String getName() {
-		A1 a1 = new A1("Kingdom");
-		return a1.defaultName;//name là default, cùng package có thể access
-	}
-	
-	int getSalary() {
-		A1 a1 = new A1("Kingdom", 123000);
-		return a1.protectedSalary;//salary là protected, cùng package cũng có thể access
+		//a1.id;//không thể access
+		a1.defaultName = "default name from class cha";
+		a1.protectedSalary = 1234;
 	}
 }
